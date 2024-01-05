5262875119e5a0ecc86a46bcbf862526421aa8f0
Peter Neher <p.neher@dkfz-heidelberg.de>
2024-01-05 14:36:53 +0100

    debug testing

--- tests/test_radtract.py
+++ tests/test_radtract.py
@@ -67,7 +67,7 @@ def test_num_parcel_esimation():
 
 def test_pyradiomics_features():
     data_folder = os.path.dirname(__file__) + '/test_data/'
-    # features_df = pd.read_pickle(data_folder + 'hyperplane_features.pkl')
+    features_df = pd.read_pickle(data_folder + 'hyperplane_features.pkl')
     pyrad_extractor = features.PyradiomicsExtractor(num_parcels=17)
     new_features = pyrad_extractor.calc_features(parcellation_file_name=data_folder + 'hyperplane_parcellation.nii.gz',
                                                  parameter_map_file_name=data_folder + 'test_map.nii.gz'
@@ -75,12 +75,6 @@ def test_pyradiomics_features():
     # remove path from 'map' and 'parcellation' columns
     new_features['map'] = new_features['map'].str.split('/').str[-1]
     new_features['parcellation'] = new_features['parcellation'].str.split('/').str[-1]
-    # new_features.to_pickle(get_results_path() + 'hyperplane_features.pkl')
-    joblib.dump(new_features, get_results_path() + 'hyperplane_features.joblib')
-
-    features_df = joblib.load(data_folder + 'hyperplane_features.joblib')
-    print('new_features.shape', new_features.shape)
-    print('features_df.shape', features_df.shape)
+    new_features.to_pickle(get_results_path() + 'hyperplane_features.pkl')
 
     pd.testing.assert_frame_equal(new_features, features_df, check_dtype=False)
-
