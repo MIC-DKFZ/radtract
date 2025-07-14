92392a93c197d8a4e252e9a70062e36c71183b8b
Peter Neher <p.neher@dkfz-heidelberg.de>
2025-07-14 07:28:46 +0200

    bump version

--- radtract/__init__.py
+++ radtract/__init__.py
@@ -1,9 +1,9 @@
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __author__ = 'Peter Neher'
 __email__ = 'p.neher@dkfz.de'
-__copyright__ = "Copyright 2023, German Cancer Research Center (DKFZ), Division of Medical Image Computing"
+__copyright__ = "Copyright 2025, German Cancer Research Center (DKFZ), Division of Medical Image Computing"
 __license__ = "Apache License 2.0"
 
-# Copyright © 2023 German Cancer Research Center (DKFZ), Division of Medical Image Computing
+# Copyright © 2025 German Cancer Research Center (DKFZ), Division of Medical Image Computing
 #
 # SPDX-License-Identifier: Apache-2.0
